dd803012fb627cfbb0b4d3b954d0435fd43db409
Yiming0576 <Yiming0576@gmail.com>
2024-10-30 14:40:20 -0400

    add the dealer class

--- src/main/java/com/pluralsight/dealershipversion2/entity/Dealer.java
+++ src/main/java/com/pluralsight/dealershipversion2/entity/Dealer.java
@@ -1,4 +1,27 @@
 package com.pluralsight.dealershipversion2.entity;
 
+import lombok.*;
+
+import java.util.List;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Dealer extends User{
+
+    private String name;
+    private String Address;
+    private String phone;
+    private VehicleInventory inventory;
+    private List<java.lang.Record> recordList;
+
+
 }
